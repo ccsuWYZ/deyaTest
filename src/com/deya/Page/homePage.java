8dc9d02036157122b4e9fef81cc77a311fcdaa2f
ccsu_wyz <3212384071@qq.com>
2023-11-22 15:41:59 +0800

    Initial commit

--- src/com/deya/Page/homePage.java
+++ src/com/deya/Page/homePage.java
@@ -9,29 +9,40 @@ import java.awt.event.ActionListener;
  * @date 2023-11-22 14:52:33
  * @description
  */
-public class homePage {
-    private JButton button1;
-    private JButton 信道建模Button;
-    private JButton 传播修正Button;
+public class homePage extends JFrame{
+
+    private JButton btn1;
+    private JButton btn2;
+    private JButton btn3;
+    private JPanel jpl;
 
     public homePage() {
-        button1.addActionListener(new ActionListener() {
+        btn1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                System.out.println("射线追踪");
 
             }
         });
-        信道建模Button.addActionListener(new ActionListener() {
+        btn2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                System.out.println("信道建模");
             }
         });
-        传播修正Button.addActionListener(new ActionListener() {
+        btn3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                System.out.println("传播修正");
             }
         });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("德雅实验室");
+        frame.setContentPane(new homePage().jpl);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800,800);
+        frame.setVisible(true);
     }
 }
